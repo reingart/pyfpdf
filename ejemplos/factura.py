6b1fd8a571df3c2cb60fe789c82786bdedb14ed9
mariano <none@none>
2008-09-24 16:24:45 +0000

    pyFPDF: ejemplo de factura
    --HG--
    branch : pyfpdf
    extra : convert_revision : svn%3Ac20948bd-09e1-4d06-97f4-0a1802b8dc70/pyfpdf%4040

---
+++ ejemplos/factura.py
@@ -0,0 +1,22 @@
+# -*- coding: iso-8859-1 -*-
+import sys,os
+sys.path.append("..")
+import FPDF
+
+cliente = {'nombre':'juan perez','cuit':'00-0000000-0','inscripto':True}
+items = [
+    {'cantidad':12,'descripcion':'Eggs','precio':1.00},
+    {'cantidad':20,'descripcion':'Spam','precio':3.00},
+    {'cantidad':1,'descripcion':'Varios','precio':0.50},
+]
+
+pdf = FPDF.FPDF()
+pdf.AddPage();
+pdf.SetFont('Arial','B',16);
+for linea in open("factura.txt").readlines():
+   args = eval(linea)
+   print linea
+   pdf.Text(x=args[0],y=args[1],txt=str(args[2]).decode('latin1'));
+
+pdf.Output(r"c:\factura.pdf","F")
+os.system(r"c:\factura.pdf")
