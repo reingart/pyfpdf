d18181b65a8fa5d3e8e28160fc62607dd9af9ace
Mariano Reingart <reingart@gmail.com>
2012-07-14 00:26:17 -0300

    moved __init__ to package dir
    --HG--
    branch : pyfpdf
    rename : __init__.py => fpdf/__init__.py

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from fpdf import FPDF
-from html import HTMLMixin
-from template import Template
---
+++ fpdf/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"FPDF for python"
+
+__license__ = "LGPL 3.0"
+__version__ = "1.7"
+
+from fpdf import *
+from html import HTMLMixin
+from template import Template
