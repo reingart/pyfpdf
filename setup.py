8d508122c283da44e6c507ad6fe364a4b767dc04
Mariano Reingart <reingart@gmail.com>
2012-07-14 00:20:00 -0300

    added setup.pya
    --HG--
    branch : pyfpdf

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+import fpdf
+
+setup(name='PyFPDF',
+      version=fpdf.__version__,
+      description='Simple PDF generation for Python',
+      author='Olivier PLATHEY ported by Max',
+      author_email='maxpat78@yahoo.it',
+      maintainer = "Mariano Reingart",
+      maintainer_email = "reingart@gmail.com",
+      url='http://code.google.com/p/pyfpdf',
+      download_url="http://pyfpdf.googlecode.com/files/pyfpdf-%s.tar.gz" % fpdf.__version__,
+      packages=['fpdf', ],
+      package_dir={'fpdf': 'fpdf'},
+      package_data={'fpdf': ['font/*.ttf']},
+      classifiers = [
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.5",
+            "Programming Language :: Python :: 2.6",
+            "Programming Language :: Python :: 2.7",
+            "Operating System :: OS Independent",
+            "Topic :: Software Development :: Libraries :: PHP Classes",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "Topic :: Multimedia :: Graphics",
+      ],
+      keywords="pdf unicode png jpg",
+     )
+
