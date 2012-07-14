b604c52ba326dcc061cc58d1aa5f65f0946f10f1
Mariano Reingart <reingart@gmail.com>
2012-07-14 14:38:48 -0300

    updated unicode tutorial / test
    --HG--
    branch : pyfpdf
    rename : tutorial/HelloWorld.txt => tests/HelloWorld.txt

---
+++ tests/ex_unicode.py
@@ -0,0 +1,29 @@
+# Example of unicode support based on tfPDF
+# http://www.fpdf.org/en/script/script92.php
+
+from fpdf import FPDF
+import sys
+
+pdf = FPDF()
+pdf.add_page()
+
+# Add a Unicode font (uses UTF-8)
+pdf.add_font('DejaVu','','DejaVuSansCondensed.ttf',uni=True)
+pdf.set_font('DejaVu','',14)
+fn = 'ex.pdf'
+
+# Load a UTF-8 string from a file and print it
+txt = open('HelloWorld.txt').read()
+pdf.write(8, txt)
+
+# Select a standard font (uses windows-1252)
+pdf.set_font('Arial','',14)
+pdf.ln(10)
+pdf.write(5, 'The file size of this PDF is only 12 KB.')
+
+pdf.output(fn,'F')
+import os
+try:
+    os.startfile(fn)
+except:
+    os.system("evince %s" % fn)
--- tutorial/unicode.py
+++ tutorial/unicode.py
@@ -9,29 +9,62 @@ fn = 'ex.pdf'
 pdf = FPDF()
 pdf.add_page()
 
-# Add a Standard Unicode font (uses UTF-8)
+# Add a DejaVu Unicode font (uses UTF-8)
+# Supports more than 200 languages. For a coverage status see:
+# http://dejavu.svn.sourceforge.net/viewvc/dejavu/trunk/dejavu-fonts/langcover.txt
 pdf.add_font('DejaVu','','DejaVuSansCondensed.ttf',uni=True)
 pdf.set_font('DejaVu','',14)
 
-# Load a UTF-8 string from a file and print it
-txt = open('HelloWorld.txt').read()
-pdf.write(8, txt)
+text = u"""
+English: Hello World
+Greek: Γειά σου κόσμος
+Polish: Witaj świecie
+Portuguese: Olá mundo
+Russian: Здравствулте мир
+Vietnamese: Xin chào thế giới
+Arabic: مرحبا العالم
+Hebrew: שלום עולם
+"""
 
+for txt in text.split('\n'):
+    pdf.write(8, txt)
+    pdf.ln(8)
 
-# Add a East-Asia Unicode font (uses UTF-8)
+# Add a Indic Unicode font (uses UTF-8)
+# Supports: Bengali, Devanagari, Gujarati, 
+#           Gurmukhi (including the variants for Punjabi) 
+#           Kannada, Malayalam, Oriya, Tamil, Telugu, Tibetan
+pdf.add_font('gargi', '', 'gargi.ttf', uni=True) 
+pdf.set_font('gargi', '', 14)
+pdf.write(8, u'Hindi: नमस्ते दुनिया')
+pdf.ln(20)
+
+# Add a AR PL New Sung Unicode font (uses UTF-8)
+# The Open Source Chinese Font (also supports others east-asia languages)
 pdf.add_font('fireflysung','','fireflysung.ttf',uni=True)
 pdf.set_font('fireflysung','',14)
-
+pdf.write(8, u'Chinese: 你好世界\n')
+pdf.write(8, u'Japanese: こんにちは世界\n')
 pdf.ln(10)
 
-pdf.write(8, 'Chinese: 你好世界\n')
-pdf.write(8, 'Japanese: こんにちは世界\n')
-pdf.write(8, 'Korean: 안녕하세요\n')
+# Add a Alee Unicode font (uses UTF-8)
+# General purpose Hangul truetype fonts that Contain Korean syllable 
+# and Latin9 (iso8859-15) characters.
+pdf.add_font('eunjin','','Eunjin.ttf',uni=True)
+pdf.set_font('eunjin','',14)
+pdf.write(8, u'Korean: 안녕하세요')
+pdf.ln(20)
+
+# Add a Fonts-TLWG (formerly ThaiFonts-Scalable) (uses UTF-8)
+pdf.add_font('waree','','Waree.ttf',uni=True)
+pdf.set_font('waree','',14)
+pdf.write(8, u'Thai: สวัสดีชาวโลก')
+pdf.ln(20)
 
 # Select a standard font (uses windows-1252)
 pdf.set_font('Arial','',14)
 pdf.ln(10)
-pdf.write(5, 'The is standard built in font')
+pdf.write(5, 'This is standard built-in font')
 
 pdf.output(fn,'F')
 import os
