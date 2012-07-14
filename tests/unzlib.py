6238ef704e630422a4cfb3d24c1fb1618094142c
Mariano Reingart <reingart@gmail.com>
2012-07-14 00:28:03 -0300

    created tests directory
    --HG--
    branch : pyfpdf

---
+++ tests/unzlib.py
@@ -0,0 +1,40 @@
+import zlib
+import sys
+import pdb
+import os
+
+print sys.argv[1]
+hex = '--hex' in sys.argv
+r = open(sys.argv[1], 'rb')
+i = 0
+length = None
+while 1:
+    l = r.readline()
+    if l == "":
+        break
+    if "/Length " in l:
+        print l
+        s = l[l.index("/Length ")+8:]
+        if ' ' in s:
+            s = s[:s.index(" ")]
+        if '>' in s:
+            s = s[:s.index(">")]
+        length = int(s)
+        print l, length
+    if l.startswith('stream') and length:
+        i += 1
+        fn = "stream_%s_%s" % (i, sys.argv[1])
+        print "writting ", length, fn
+        s = r.read(length)
+        w = open(fn, 'wb')
+        try:
+            s = zlib.decompress(s)
+        except zlib.error:
+            pass
+        if hex:
+            s = s.encode('hex')
+        w.write(s)
+        w.close()
+r.close()
+
+os.system("windiff stream_1_ex_php.pdf stream_1_ex.pdf")
