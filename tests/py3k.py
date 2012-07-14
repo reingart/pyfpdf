7b7c70243eaee73a50b6ccca4821efa78d9846f4
Mariano Reingart <reingart@gmail.com>
2012-07-14 18:27:28 -0300

    added py3k basic test
    --HG--
    branch : pyfpdf

---
+++ tests/py3k.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+
+"Basic example to test py3k conversion"
+
+import sys
+from fpdf import FPDF
+    
+pdf = FPDF()
+# compression is not yet supported in py3k version
+pdf.compress = False
+pdf.add_page()
+# unicode is not yet supported in py3k version, use windows-1252 standards font
+pdf.set_font('Arial','',14)  
+pdf.ln(10)
+pdf.write(5, 'hello world %s αινσϊόρ' % sys.version)
+
+fn='py3k.pdf'
+pdf.output(fn,'F')
+import os
+try:
+    os.startfile(fn)
+except:
+    os.system("evince %s" % fn)
