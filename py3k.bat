d9066732b088561945893c7506348d5d38ba0078
Mariano Reingart <reingart@gmail.com>
2014-02-01 18:23:55 -0300

    removed conversions scripts to py3k (not needed anymore)

--- py3k.bat
+++
@@ -1,6 +0,0 @@
-rem Convert to Python 3.x
-
-mkdir fpdf_py3k
-c:\python32\tools\Scripts\2to3.py -f all -w -o fpdf_py3k -n fpdf 
-c:\Python32\python.exe setup.py install
-c:\Python32\python.exe tests\py3k.py
