6a58a5e0b1924ba9f8a440c5a93cbba4714f23ff
Mariano Reingart <reingart@gmail.com>
2014-02-01 15:13:23 -0300

    removed import * (py2.5)

--- fpdf/__init__.py
+++ fpdf/__init__.py
@@ -6,7 +6,7 @@
 __license__ = "LGPL 3.0"
 __version__ = "1.7.2"
 
-from .fpdf import *
+from .fpdf import FPDF, FPDF_FONT_DIR, FPDF_VERSION, SYSTEM_TTFONTS, set_global
 try:
     from .html import HTMLMixin
 except ImportError:
