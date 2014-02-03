394a3ce749f0bdfd8e7029554d13d09fc258550f
Roman Kharin <romiq.kh@gmail.com>
2014-02-03 12:02:29 +0400

    tests: fix issue33 test for pillow

--- tests/cover/test_issue33.py
+++ tests/cover/test_issue33.py
@@ -23,35 +23,35 @@ except ImportError:
 
 def genbar():
     # bg
-    bg = Image.new("L", (112, 1), 255)
+    bg = Image.new("L", (112, 1), 1)
     bg = bg.resize((112, 11))
     # stripes
     vbarnum = [0x4F43484B, 0xEE90D642, 0xF11A2735, 0xD71A]
-    vbar = Image.new("L", (112, 1), 192)
+    vbar = Image.new("L", (112, 1), 1)
     pix = vbar.load()
     pos = 0
     for b in vbarnum:
         for i in range(32):
-            pix[pos, 0] = 0 if (b & 1) else 255
+            pix[pos, 0] = 0 if (b & 1) else 1
             b = b >> 1
             pos += 1
             if pos >= 112: break    
     vbar = vbar.resize((112, 31), Image.NEAREST)
     # digit
     dignum = [0x398, 0x6dc, 0x61a, 0x31b, 0x1bf, 0x6d8, 0x7d8]
-    dbar = Image.new("L", (16, 7), 160)
+    dbar = Image.new("L", (16, 7), 1)
     pix = dbar.load()
     pos = 0
     ypos = 0
     for b in dignum:
         for i in range(16):
-            pix[pos, ypos] = 0 if (b & 1) else 255
+            pix[pos, ypos] = 0 if (b & 1) else 1
             b = b >> 1
             pos += 1
         ypos += 1
         pos = 0
     # result
-    bar = Image.new("L", (114, 44), 128)
+    bar = Image.new("L", (114, 44), 2)
     bar.paste(vbar, (1, 1))
     bar.paste(bg, (1, 32))
     for i in range(4):
@@ -61,11 +61,9 @@ def genbar():
 
 def dotest(outputname, nostamp):
     plane = genbar()
-    pal_image = Image.new("P", (1,1))
-    pal_image.putpalette( (0,0,0, 255,255,255) + (128,128,128)*254)
-
-    img = Image.merge("RGB", (plane, plane, plane))
-    img = img.quantize(palette = pal_image)
+    palette = (0,0,0, 255,255,255) + (128,128,128)*254
+    img = Image.fromstring("P", plane.size, plane.tostring())
+    img.putpalette(palette)
 
     f = tempfile.NamedTemporaryFile(delete = False, suffix = ".gif")
     gif1 = f.name
