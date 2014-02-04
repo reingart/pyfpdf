83492de9f46ecaa416ac493044f5086fb175dc95
Mariano Reingart <reingart@gmail.com>
2014-02-04 05:05:28 -0300

    added test images from Pillow for py3k test, and minor changes

--- tests/py3k.py
+++ tests/py3k.py
@@ -8,13 +8,15 @@ from fpdf import FPDF
     
 pdf = FPDF()
 # compression is not yet supported in py3k version
-pdf.compress = False
+pdf.compress = True
 pdf.add_page()
 # unicode is not yet supported in py3k version, use windows-1252 standards font
 pdf.set_font('Arial','',14)  
 pdf.ln(10)
 pdf.write(5, 'hello world %s αινσϊόρ' % sys.version)
 pdf.image("../tutorial/logo.png", 50, 50)
+pdf.image("flower2.jpg", 100, 50)
+pdf.image("lena.gif", 50, 75)
 
 fn='py3k.pdf'
 pdf.output(fn,'F')
