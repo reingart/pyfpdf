3d0cd8beaf536fa4088c76207264776b2b58c05d
Mariano Reingart <reingart@gmail.com>
2014-02-04 21:48:29 -0300

    added TTF unicode font to py3k test

--- tests/py3k.py
+++ tests/py3k.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 
 "Basic example to test py3k conversion"
 
@@ -7,16 +7,24 @@ import sys
 from fpdf import FPDF
     
 pdf = FPDF()
-# compression is not yet supported in py3k version
+# compression also supported in py3k version
 pdf.compress = True
 pdf.add_page()
 # unicode is not yet supported in py3k version, use windows-1252 standards font
 pdf.set_font('Arial','',14)  
 pdf.ln(10)
-pdf.write(5, 'hello world %s �������' % sys.version)
+pdf.write(5, u'hello world %s áéíóúüñ' % sys.version)
 pdf.image("../tutorial/logo.png", 50, 50)
 pdf.image("flower2.jpg", 100, 50)
 pdf.image("lena.gif", 50, 75)
+
+# Add a DejaVu Unicode font (uses UTF-8)
+# Supports more than 200 languages. For a coverage status see:
+# http://dejavu.svn.sourceforge.net/viewvc/dejavu/trunk/dejavu-fonts/langcover.txt
+pdf.add_font('DejaVu','','DejaVuSansCondensed.ttf',uni=True)
+pdf.set_font('DejaVu','',14)
+pdf.ln(10)
+pdf.write(8, u"Hello world in Russian: Здравствулте мир")
 
 fn='py3k.pdf'
 pdf.output(fn,'F')
