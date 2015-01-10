6f1599cc9b62524c6d26407ac5377a7c4c5d4aaa
Roman Kharin <romiq.kh@gmail.com>
2015-01-10 12:53:03 +0300

    batch tests: remove unrequired PIL depency from issue14 and issue71

--- tests/cover/test_issue71.py
+++ tests/cover/test_issue71.py
@@ -5,7 +5,6 @@
 #PyFPDF-cover-test:format=PDF
 #PyFPDF-cover-test:fn=issue_71.pdf
 #PyFPDF-cover-test:hash=1575947cac5b0a8cdceedf9b525ee6db
-#PyFPDF-cover-test:pil=yes
 # get res from http://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Code_3_of_9.svg/262px-Code_3_of_9.svg.png
 # PyFPDF-cover-test:res=.png
 
