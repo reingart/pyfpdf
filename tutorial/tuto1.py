78a4a0fac7213d2932b0dde167e0c370e82d1f03
Roman Kharin <romiq.kh@gmail.com>
2015-04-06 11:57:48 +0300

    Make tutorials code PEP-8 compliant, fix tutorial errors

--- tutorial/tuto1.py
+++ tutorial/tuto1.py
@@ -1,7 +1,8 @@
 from fpdf import FPDF
 
-pdf=FPDF()
+pdf = FPDF()
 pdf.add_page()
-pdf.set_font('Arial','B',16)
-pdf.cell(40,10,'Hello World!')
-pdf.output('tuto1.pdf','F')
+pdf.set_font('Arial', 'B', 16)
+pdf.cell(40, 10, 'Hello World!')
+pdf.output('tuto1.pdf', 'F')
+
--- tutorial/unicode.py
+++ tutorial/unicode.py
@@ -12,8 +12,8 @@ pdf.add_page()
 # Add a DejaVu Unicode font (uses UTF-8)
 # Supports more than 200 languages. For a coverage status see:
 # http://dejavu.svn.sourceforge.net/viewvc/dejavu/trunk/dejavu-fonts/langcover.txt
-pdf.add_font('DejaVu','','DejaVuSansCondensed.ttf',uni=True)
-pdf.set_font('DejaVu','',14)
+pdf.add_font('DejaVu', '', 'DejaVuSansCondensed.ttf', uni = True)
+pdf.set_font('DejaVu', '', 14)
 
 text = u"""
 English: Hello World
@@ -41,8 +41,8 @@ pdf.ln(20)
 
 # Add a AR PL New Sung Unicode font (uses UTF-8)
 # The Open Source Chinese Font (also supports others east-asia languages)
-pdf.add_font('fireflysung','','fireflysung.ttf',uni=True)
-pdf.set_font('fireflysung','',14)
+pdf.add_font('fireflysung', '','fireflysung.ttf', uni = True)
+pdf.set_font('fireflysung', '', 14)
 pdf.write(8, u'Chinese: 你好世界\n')
 pdf.write(8, u'Japanese: こんにちは世界\n')
 pdf.ln(10)
@@ -50,25 +50,26 @@ pdf.ln(10)
 # Add a Alee Unicode font (uses UTF-8)
 # General purpose Hangul truetype fonts that Contain Korean syllable 
 # and Latin9 (iso8859-15) characters.
-pdf.add_font('eunjin','','Eunjin.ttf',uni=True)
-pdf.set_font('eunjin','',14)
+pdf.add_font('eunjin', '', 'Eunjin.ttf', uni = True)
+pdf.set_font('eunjin','', 14)
 pdf.write(8, u'Korean: 안녕하세요')
 pdf.ln(20)
 
 # Add a Fonts-TLWG (formerly ThaiFonts-Scalable) (uses UTF-8)
-pdf.add_font('waree','','Waree.ttf',uni=True)
-pdf.set_font('waree','',14)
+pdf.add_font('waree', '', 'Waree.ttf', uni = True)
+pdf.set_font('waree', '', 14)
 pdf.write(8, u'Thai: สวัสดีชาวโลก')
 pdf.ln(20)
 
 # Select a standard font (uses windows-1252)
-pdf.set_font('Arial','',14)
+pdf.set_font('Arial', '', 14)
 pdf.ln(10)
 pdf.write(5, 'This is standard built-in font')
 
-pdf.output(fn,'F')
+pdf.output(fn, 'F')
 import os
 try:
     os.startfile(fn)
 except:
-    os.system("evince %s" % fn)
+    os.system("xdg-open \"%s\"" % fn)
+
