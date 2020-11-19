fa5620ce7a8d09f748d9dccc345822727c51b4c1
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2020-11-19 19:18:53 +0100

    Performance boost for image_parsing.get_img_info

--- fpdf/image_parsing.py
+++ fpdf/image_parsing.py
@@ -37,15 +37,19 @@ def get_img_info(bytesio):
         img = img.convert('RGBA')
     w, h = img.size
     info = {}
-    if img.mode in ('L', 'LA'):
+    if img.mode == 'L':
         dpn, bpc, colspace = 1, 8, 'DeviceGray'
-        info['data'] = to_zdata(img, slice(0, 1))
-        if img.mode == 'LA':
-            info['smask'] = to_zdata(img, slice(1, 2))
+        info['data'] = to_zdata(img)
+    elif img.mode == 'LA':
+        dpn, bpc, colspace = 1, 8, 'DeviceGray'
+        alpha_channel = slice(1, None, 2)
+        info['data'] = to_zdata(img, remove_slice=alpha_channel)
+        info['smask'] = to_zdata(img, select_slice=alpha_channel)
     else:  # RGBA image
         dpn, bpc, colspace = 3, 8, 'DeviceRGB'
-        info['data'] = to_zdata(img, slice(0, 3))
-        info['smask'] = to_zdata(img, slice(3, 4))
+        alpha_channel = slice(3, None, 4)
+        info['data'] = to_zdata(img, remove_slice=alpha_channel)
+        info['smask'] = to_zdata(img, select_slice=alpha_channel)
 
     dp = '/Predictor 15 /Colors ' + str(dpn) + ' /BitsPerComponent '+str(bpc)+' /Columns '+str(w)+''
 
@@ -63,12 +67,17 @@ def get_img_info(bytesio):
     return info
 
 
-def to_zdata(img, slice_=slice(0, None)):
-    is_single_channel = isinstance(img.getdata()[0], int)
-    assert not is_single_channel or slice_ in (slice(0, None), slice(0, 1)), 'Single-channel images cannot be sliced'
-    data = b''
-    for i, px in enumerate(img.getdata()):
-        if i % img.size[0] == 0:
-            data += b'\0'
-        data += bytes([px] if is_single_channel else px[slice_])
+def to_zdata(img, remove_slice=None, select_slice=None):
+    data = bytearray(img.tobytes())
+    if remove_slice:
+        del data[remove_slice]
+    if select_slice:
+        data = data[select_slice]
+    # Left-padding every row with a single zero:
+    channels_count = len(data) // (img.size[0] * img.size[1])
+    loop_incr = img.size[0] * channels_count + 1
+    i = 0
+    while i < len(data):
+        data[i:i] = b'\0'
+        i += loop_incr
     return zlib.compress(data)
