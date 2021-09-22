reingart/pyfpdf

commit 3956e0e44f66dbbfdaa3fd957450cfbca66dc521

Fix: HTML headings can now contain non-ASCII characters without triggering a UnicodeEncodeError - close #220 (#222)

fpdf/syntax.py
@@ -198,7 +198,7 @@ def camel_case(property_name):
 
 class PDFString(str):
     def serialize(self):
-        return f"({self})"
+        return f'({self.encode("UTF-16BE").decode("latin-1")})'
 
 
 class PDFArray(list):

commit cfe929043bf77ab0173f7a02f0ace145dd879f73

Fixing outline heading section names that have been broken by PR #222

fpdf/syntax.py
@@ -198,7 +198,9 @@ def camel_case(property_name):
 
 class PDFString(str):
     def serialize(self):
-        return f'({self.encode("UTF-16BE").decode("latin-1")})'
+        # Filtering out characters that are not encodable as Latin1 for now,
+        # as an outline /Title seemingly cannot "just" be encoded as UTF-16BE:
+        return f'({self.encode("latin-1", "ignore").decode("latin-1")})'
 
 
 class PDFArray(list):

test/outline/test_outline_serializer.py
@@ -18,7 +18,7 @@ def test_serialize_outline():
     )
     assert (
         serialize_outline(sections, first_object_id=6)
-        == f"""\
+        == """\
 6 0 obj
 <<
 /Count 2
@@ -35,7 +35,7 @@ endobj
 /Last 8 0 R
 /Next 9 0 R
 /Parent 6 0 R
-/Title ({'Title 1'.encode("UTF-16BE").decode("latin-1")})
+/Title (Title 1)
 >>
 endobj
 8 0 obj
@@ -43,7 +43,7 @@ endobj
 /Count 0
 /Dest [5 0 R /XYZ 0 0 null]
 /Parent 7 0 R
-/Title ({'Subtitle 1.1'.encode("UTF-16BE").decode("latin-1")})
+/Title (Subtitle 1.1)
 >>
 endobj
 9 0 obj
@@ -54,7 +54,7 @@ endobj
 /Last 11 0 R
 /Parent 6 0 R
 /Prev 7 0 R
-/Title ({'Title 2'.encode("UTF-16BE").decode("latin-1")})
+/Title (Title 2)
 >>
 endobj
 10 0 obj
@@ -63,7 +63,7 @@ endobj
 /Dest [9 0 R /XYZ 0 0 null]
 /Next 11 0 R
 /Parent 9 0 R
-/Title ({'Subtitle 2.1'.encode("UTF-16BE").decode("latin-1")})
+/Title (Subtitle 2.1)
 >>
 endobj
 11 0 obj
@@ -72,7 +72,7 @@ endobj
 /Dest [11 0 R /XYZ 0 0 null]
 /Parent 9 0 R
 /Prev 10 0 R
-/Title ({'Subtitle 2.2'.encode("UTF-16BE").decode("latin-1")})
+/Title (Subtitle 2.2)
 >>
 endobj"""
     )

test/test_structure_tree.py
@@ -81,11 +81,9 @@ def test_single_image_structure_tree():
     struct_builder.add_marked_content(
         MarkedContent(1, 0, "/Figure", 0, "Image title", "Image description")
     )
-    encoded_desc = "Image description".encode("UTF-16BE").decode("latin-1")
-    encoded_title = "Image title".encode("UTF-16BE").decode("latin-1")
     assert (
         struct_builder.serialize(first_object_id=3)
-        == f"""\
+        == """\
 3 0 obj
 <<
 /K [4 0 R]
@@ -108,12 +106,12 @@ endobj
 endobj
 6 0 obj
 <<
-/Alt ({encoded_desc})
+/Alt (Image description)
 /K [0]
 /P 4 0 R
 /Pg 1 0 R
 /S /Figure
-/T ({encoded_title})
+/T (Image title)
 /Type /StructElem
 >>
 endobj"""