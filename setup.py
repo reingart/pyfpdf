bbe32843da4f802718f3a8a56ce7ed5c2adc7a49
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2022-09-07 22:13:33 +0200

    Extra Python 3.10 classifier added

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
             "Operating System :: OS Independent",
             "Topic :: Printing",
             "Topic :: Software Development :: Libraries :: Python Modules",
