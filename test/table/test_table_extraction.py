b43f0cefe9ddbb1bd1b45cf424e073a0d9e86a83
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2023-04-03 16:55:54 +0200

    Fixing ModuleNotFoundError: No module named 'pandas.util.testing'

--- test/table/test_table_extraction.py
+++ test/table/test_table_extraction.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 import camelot
 from pandas import DataFrame
-from pandas.util.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal
 import pytest
 import tabula
 
